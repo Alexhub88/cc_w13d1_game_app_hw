e7ce0180a929e0145e3b327969ec28df48cf3bf5
Alexhub88 <alexscott88@hotmail.co.uk>
2018-05-28 16:34:03 +0100

    Homework answers in mongo_hw.js

---
+++ mongo_hw.js
@@ -0,0 +1,6 @@
+1: games_router.js
+2: Using the index_router file which tells the gamesRouter object the URL path to follow (line 13)
+3: Sets up the index router (line 11), and points the app to the public files (line 8)
+4: Points the server to the gamesRouter resource
+5: Defines the routes for the 'games' model
+6: GET, POST, DELETE, PUT
